73e88b1ddf6060d9d4706fe5a2c64828782127fc
RitikSingh2005 <hritiksingh5002@gmail.com>
2026-02-19 16:20:28 +0530

    initial commit

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,22 +1,22 @@
-import { useUser } from "@clerk/clerk-react";
-import React from "react";
+import { useUser, RedirectToSignIn } from "@clerk/clerk-react";
 import { Navigate, useLocation } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const { isSignedIn, user, isLoaded } = useUser();
-  const { pathname } = useLocation();
+  const location = useLocation();
 
-  if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
-    return <Navigate to="/?sign-in=true" />;
+  if (!isLoaded) return <div>Loading...</div>;
+
+  if (!isSignedIn) {
+    return <RedirectToSignIn />;
   }
 
-  // check onboarding status
   if (
-    user !== undefined &&
     !user?.unsafeMetadata?.role &&
-    pathname !== "/onboarding"
-  )
-    return <Navigate to="/onboarding" />;
+    location.pathname !== "/onboarding"
+  ) {
+    return <Navigate to="/onboarding" replace />;
+  }
 
   return children;
 };
